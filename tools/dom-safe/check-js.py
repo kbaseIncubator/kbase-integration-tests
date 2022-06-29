a0d6458a23aea6556f6aefe7df162b9d00e34e09
Erik Pearson <eapearson@lbl.gov>
2022-06-29 07:39:32 -0700

    add option to omit jquery tests helps with repos which don't use jquery and the jquery test produces false positives

--- tools/dom-safe/check-js.py
+++ tools/dom-safe/check-js.py
@@ -1,7 +1,7 @@
-import sys
 import glob
-import re
 import os
+import re
+import sys
 
 EXTENSIONS = [
     'js',
@@ -10,13 +10,15 @@ EXTENSIONS = [
     'tsx'
 ]
 
-VERBOSE=False
+VERBOSE = False
+
 
 def get_files(extensions, dir_to_check):
     files = []
     for extension in extensions:
         files.extend(glob.iglob(f'./**/*.{extension}', root_dir=dir_to_check, recursive=True))
     return files
+
 
 def check_inner_html(dir_to_check, show_files=False, omit_pattern=None):
     print('')
@@ -38,8 +40,8 @@ def check_inner_html(dir_to_check, show_files=False, omit_pattern=None):
         if os.path.isdir(file_path):
             continue
 
-        if omit_pattern is not None and re.search(omit_pattern, file): 
-                continue
+        if omit_pattern is not None and re.search(omit_pattern, file):
+            continue
 
         with open(file_path) as fin:
             lines = fin.readlines()
@@ -52,7 +54,7 @@ def check_inner_html(dir_to_check, show_files=False, omit_pattern=None):
                     if re.search('^(\s)*//', line):
                         comment_count += 1
                         handled = True
-                     # check if safe (prev line contains a comment )
+                    # check if safe (prev line contains a comment )
                     elif prev_line is not None and re.search('^(\s)*//\s*xss\s*safe', prev_line):
                         safe_count += 1
                         handled = True
@@ -97,30 +99,129 @@ def check_inner_html(dir_to_check, show_files=False, omit_pattern=None):
             print(f'{filename}: {record["count"]}')
 
         if show_files:
-                if len(sorted_files) > 0:
-                    filename, record = sorted_files[len(sorted_files) - 1]
-                    print(f'File: {filename}')
-                    print('----')
-                    for line_stats in record['lines']:
-                        print(f'[{line_stats["line_number"] + 1}] {line_stats["line"]}')
+            if len(sorted_files) > 0:
+                filename, record = sorted_files[len(sorted_files) - 1]
+                print(f'File: {filename}')
+                print('----')
+                for line_stats in record['lines']:
+                    print(f'[{line_stats["line_number"] + 1}] {line_stats["line"]}')
 
-    return [lines_to_investigate_count, 
-        {
-            'stats': {
-                'total': {
-                    'candidate_lines': candidate_line_count,
-                    'lines_to_investigate': lines_to_investigate_count
-                },
-                'safe_usages_annotated': {
-                    'safe': safe_count
-                },
-                'safe_usages_detected': {
-                    'simple_string': simple_string_count,
-                    'return_value': return_value_count
+    return [lines_to_investigate_count,
+            {
+                'stats': {
+                    'total': {
+                        'candidate_lines': candidate_line_count,
+                        'lines_to_investigate': lines_to_investigate_count
+                    },
+                    'safe_usages_annotated': {
+                        'safe': safe_count
+                    },
+                    'safe_usages_detected': {
+                        'simple_string': simple_string_count,
+                        'return_value': return_value_count
+                    }
                 }
             }
-        }
-    ]
+            ]
+
+
+def check_knockout_usage(dir_to_check, show_files=False, omit_pattern=None):
+    print('')
+    print('Checking for knockoutjs usage...')
+    if omit_pattern is not None:
+        print(f'Using omit pattern {omit_pattern}')
+
+    files = get_files(EXTENSIONS, dir_to_check)
+
+    comment_count = 0
+    safe_count = 0
+    lines_to_investigate_count = 0
+    candidate_line_count = 0
+    simple_string_count = 0
+    return_value_count = 0
+    files_to_fix = {}
+    for file in files:
+        file_path = f'{dir_to_check}/{file}'
+        if os.path.isdir(file_path):
+            continue
+
+        if omit_pattern is not None and re.search(omit_pattern, file):
+            continue
+
+        with open(file_path) as fin:
+            lines = fin.readlines()
+            prev_line = None
+            for line_number, line in enumerate(lines):
+                handled = False
+                if 'html:' in line:
+                    candidate_line_count += 1
+                    # skip lines which are comments (start with //)
+                    if re.search('^(\s)*//', line):
+                        comment_count += 1
+                        handled = True
+                    # check if safe (prev line contains a comment with xss safe)
+                    elif prev_line is not None and re.search('^(\s)*//\s*xss\s*safe', prev_line):
+                        safe_count += 1
+                        handled = True
+
+                    else:
+                        lines_to_investigate_count += 1
+
+                    if not handled:
+                        lines_to_investigate_count += 1
+                        if file not in files_to_fix:
+                            files_to_fix[file] = {
+                                'count': 1,
+                                'lines': [{
+                                    'line_number': line_number,
+                                    'line': line
+                                }]
+                            }
+                        else:
+                            files_to_fix[file]['count'] += 1
+                            files_to_fix[file]['lines'].append({
+                                'line_number': line_number,
+                                'line': line
+                            })
+                prev_line = line
+
+    if VERBOSE or lines_to_investigate_count > 0:
+        print(f'Found: {lines_to_investigate_count}')
+        print(f'Comments: {comment_count}')
+        print(f'Safe: {safe_count}')
+
+        total = 0
+        sorted_files = sorted(files_to_fix.items(), key=lambda item: item[1]['count'])
+        for filename, record in sorted_files:
+            total += record['count']
+            print(f'{filename}: {record["count"]}')
+
+        if show_files:
+            if len(sorted_files) > 0:
+                filename, record = sorted_files[len(sorted_files) - 1]
+                print(f'File: {filename}')
+                print('----')
+                for line_stats in record['lines']:
+                    print(f'[{line_stats["line_number"] + 1}] {line_stats["line"]}')
+
+    return [lines_to_investigate_count,
+            {
+                'stats': {
+                    'total': {
+                        'candidate_lines': candidate_line_count,
+                        'lines_to_investigate': lines_to_investigate_count
+                    },
+                    'safe_usages_annotated': {
+                        'safe': safe_count
+                    },
+                    'safe_usages_detected': {
+                        'simple_string': simple_string_count,
+                        'return_value': return_value_count
+                    }
+                }
+            }
+            ]
+
 
 def check_react_usage(dir_to_check, show_files=True, omit_pattern=None):
     print('')
@@ -143,14 +244,14 @@ def check_react_usage(dir_to_check, show_files=True, omit_pattern=None):
             continue
 
         # omit files according to a pattern
-        if omit_pattern is not None and re.search(omit_pattern, file): 
+        if omit_pattern is not None and re.search(omit_pattern, file):
             continue
 
         with open(f'{dir_to_check}/{file}') as fin:
             lines = fin.readlines()
-            
+
             prev_line = None
-            
+
             for line_number, raw_line in enumerate(lines):
                 line = raw_line.rstrip()
                 handled = False
@@ -207,7 +308,8 @@ def check_react_usage(dir_to_check, show_files=True, omit_pattern=None):
         if lines_to_investigate_count == 0:
             print('Nothing to investigate')
         else:
-            print(f'{lines_to_investigate_count} instance{"" if lines_to_investigate_count == 1 else "s"} to investigate')
+            print(
+                f'{lines_to_investigate_count} instance{"" if lines_to_investigate_count == 1 else "s"} to investigate')
             print('')
 
             total = 0
@@ -246,14 +348,18 @@ def check_react_usage(dir_to_check, show_files=True, omit_pattern=None):
         }
     ]
 
+
 def is_block_comment_start(line):
     return re.search('^(\s)*[\/][*]', line)
+
 
 def is_block_comment_end(line):
     return re.search('[*][\/]\s*$', line)
 
+
 def is_line_comment(line):
     return re.search('^(\s)*//', line)
+
 
 def check_jquery_function(jquery_methods, dir_to_check, show_files=True, omit_pattern=None):
     print('')
@@ -287,7 +393,7 @@ def check_jquery_function(jquery_methods, dir_to_check, show_files=True, omit_pa
             continue
 
         # omit files according to a pattern
-        if omit_pattern is not None and re.search(omit_pattern, file): 
+        if omit_pattern is not None and re.search(omit_pattern, file):
             continue
 
         total_file_count += 1
@@ -298,9 +404,9 @@ def check_jquery_function(jquery_methods, dir_to_check, show_files=True, omit_pa
             lines = fin.readlines()
 
             total_line_count += len(lines)
-            
+
             prev_line = None
-            
+
             for line_number, raw_line in enumerate(lines):
                 line = raw_line.rstrip()
 
@@ -313,9 +419,9 @@ def check_jquery_function(jquery_methods, dir_to_check, show_files=True, omit_pa
                         block_comment_line_count += 1
 
                 if is_line_comment(line):
-                        line_comment_count += 1
-                        prev_line = line
-                        continue
+                    line_comment_count += 1
+                    prev_line = line
+                    continue
 
                 if is_block_comment_start(line):
                     block_comment_count += 1
@@ -358,15 +464,18 @@ def check_jquery_function(jquery_methods, dir_to_check, show_files=True, omit_pa
                                 elif re.search(f'{jquery_method}[(]\s*["][^"<>]*["]\s*[)]', line):
                                     simple_string_count += 1
                                 # A quoted string with a simple tag, but not script.
-                                elif re.search(f"{jquery_method}[(]['][<][a-z]+[\/]?[>]['][)]", line) and not re.search('script', line):
+                                elif re.search(f"{jquery_method}[(]['][<][a-z]+[\/]?[>]['][)]", line) and not re.search(
+                                        'script', line):
                                     simple_tag_count += 1
                                 # A quoted string with a simple tag, but not script.
-                                elif re.search(f'{jquery_method}[(]["][<][a-z]+[\/]?[>]["][)]', line) and not re.search('script', line):
+                                elif re.search(f'{jquery_method}[(]["][<][a-z]+[\/]?[>]["][)]', line) and not re.search(
+                                        'script', line):
                                     simple_tag_count += 1
                                 # A simple jquery object which itself takes a simple tag which 
                                 # doesn't contain "script"
                                 # e.g. .html($('<div>')) or .append($('<span/>'))
-                                elif re.search(f"{jquery_method}[(][$][(]['][<][a-z]+[\/]?[>]['][)]", line) and not re.search('script', line):
+                                elif re.search(f"{jquery_method}[(][$][(]['][<][a-z]+[\/]?[>]['][)]",
+                                               line) and not re.search('script', line):
                                     simple_tag_count += 1
                                 # Various usages of functions which ensure safe usage.
                                 elif re.search(f"{jquery_method}[(]domSafeText[(].*[)][)]", line):
@@ -429,7 +538,8 @@ def check_jquery_function(jquery_methods, dir_to_check, show_files=True, omit_pa
         if lines_to_investigate_count == 0:
             print('Nothing to investigate')
         else:
-            print(f'{lines_to_investigate_count} instance{"" if lines_to_investigate_count == 1 else "s"} to investigate')
+            print(
+                f'{lines_to_investigate_count} instance{"" if lines_to_investigate_count == 1 else "s"} to investigate')
             print('')
 
             total = 0
@@ -451,33 +561,33 @@ def check_jquery_function(jquery_methods, dir_to_check, show_files=True, omit_pa
                         print(f'[{line_stats["line_number"] + 1}] {line_stats["line"]}')
 
     return [lines_to_investigate_count, {
-            'stats': {
-                'total': {
-                    'files': total_file_count,
-                    'lines': total_line_count,
-                    'lines_analyzed': total_lines_analyzed,
-                    'candidate_lines': candidate_line_count,
-                    'lines_to_investigate': lines_to_investigate_count
-                },
-                'skipped': {
-                    'line_comments': line_comment_count,
-                    'block_comments': block_comment_count,
-                    'block_comment_lines': block_comment_line_count
-                },
-                'safe_usages_detected': {
-                    'simple_string': simple_string_count,
-                    'simple_tag': simple_tag_count,
-                    'domSafeText': dom_safe_text,
-                    '$errorAlert': error_alert_count,
-                    'DOMPurify.sanitize': purified_text
-                },
-                'safe_usages_annotated': {
-                    'safe': safe_count,
-                    'ignored': ignore_count
-                }
+        'stats': {
+            'total': {
+                'files': total_file_count,
+                'lines': total_line_count,
+                'lines_analyzed': total_lines_analyzed,
+                'candidate_lines': candidate_line_count,
+                'lines_to_investigate': lines_to_investigate_count
+            },
+            'skipped': {
+                'line_comments': line_comment_count,
+                'block_comments': block_comment_count,
+                'block_comment_lines': block_comment_line_count
+            },
+            'safe_usages_detected': {
+                'simple_string': simple_string_count,
+                'simple_tag': simple_tag_count,
+                'domSafeText': dom_safe_text,
+                '$errorAlert': error_alert_count,
+                'DOMPurify.sanitize': purified_text
+            },
+            'safe_usages_annotated': {
+                'safe': safe_count,
+                'ignored': ignore_count
             }
         }
-    ]
+    }
+            ]
 
 
 def update_stats(total_stats, stats):
@@ -490,22 +600,30 @@ def update_stats(total_stats, stats):
             else:
                 update_stats(total_stats[key], value)
 
+
 def print_stats(stats, indent=''):
     for key, value in stats.items():
         if isinstance(stats[key], int):
             print(f'{indent}{key}: {value:,}')
         else:
             print(f'{indent}{key}')
-            print_stats(stats[key], indent = indent + '    ')
+            print_stats(stats[key], indent=indent + '    ')
+
 
 def main():
     dir_to_check = sys.argv[1]
     print(f'Checking files ({", ".join(EXTENSIONS)}) in {dir_to_check}')
-    if len(sys.argv) == 3:
+    if len(sys.argv) == 4:
         omit_pattern = sys.argv[2]
+        omit_tests = sys.argv[3]
+        print(f'Omitting {omit_pattern}')
+    elif len(sys.argv) == 3:
+        omit_pattern = sys.argv[2]
+        omit_tests = None
         print(f'Omitting {omit_pattern}')
     else:
         omit_pattern = None
+        omit_tests = None
         print(f'without omitting anything')
 
     show_files = True
@@ -516,24 +634,30 @@ def main():
     # These appear to be all the jquery methods which can insert raw html strings into the DOM.
     # see https://api.jquery.com/category/manipulation/ 
 
-    jquery_methods = [
-        'append', 'html', 'prepend', 'appendTo', 'prependTo', 'after', 'before', 'insertAfter', 'insertBefore', 'replaceAll', 'replaceWith', 'wrap', 'wrapAll'
-    ]
-    [error_count, stats] = check_jquery_function(jquery_methods, dir_to_check, show_files=True, omit_pattern=omit_pattern)
-    total_errors += error_count
-    update_stats(total_stats, stats)
-
+    if omit_tests is None or 'jquery' not in omit_tests:
+        jquery_methods = [
+            'append', 'html', 'prepend', 'appendTo', 'prependTo', 'after', 'before', 'insertAfter', 'insertBefore',
+            'replaceAll', 'replaceWith', 'wrap', 'wrapAll'
+        ]
+        [error_count, stats] = check_jquery_function(jquery_methods, dir_to_check, show_files=True,
+                                                     omit_pattern=omit_pattern)
+        total_errors += error_count
+        update_stats(total_stats, stats)
 
     # This checks for raw usage of innerHTML.
     [error_count, stats] = check_inner_html(dir_to_check, show_files, omit_pattern=omit_pattern)
     total_errors += error_count
     update_stats(total_stats, stats)
 
+    if omit_tests is None or 'knockout' not in omit_tests:
+        [error_count, stats] = check_knockout_usage(dir_to_check, show_files, omit_pattern=omit_pattern)
+        total_errors += error_count
+        update_stats(total_stats, stats)
+
     # This checks for preact usage.
     [error_count, stats] = check_react_usage(dir_to_check, show_files, omit_pattern=omit_pattern)
     total_errors += error_count
     update_stats(total_stats, stats)
-
 
     print('')
     print('===================')
@@ -551,5 +675,6 @@ def main():
 
     print_stats(total_stats)
 
-if __name__ == '__main__':  
+
+if __name__ == '__main__':
     main()
